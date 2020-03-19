9d578cd72b136dca91a6d014ea73e1df717a0ca5
IvyWesselink <55988292+IvyWesselink@users.noreply.github.com>
2020-03-20 00:36:13 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,61 @@
+// this is your custom javascript code
+let xmlns = "http://www.w3.org/2000/svg";
+let svg = document.createElementNS(xmlns, "svg");
+svg.setAttribute("width", 720);
+svg.setAttribute("height", 360);
+svg.setAttribute("viewbox", '0 0 720 360');
+
+d3.select(window).on("load", changeCircle);
+
+d3.select("#btn-wrapper")
+.append("button")
+.text("Circles")
+.on("click", changeCircle)
+
+
+d3.select("#btn-wrapper")
+.append("button")
+.text("Squares")
+.on("click", changeRect)
+
+document.body.appendChild(svg);
+
+
+function changeRect(){
+
+    d3.selectAll("circle").remove()
+
+    for(let i=0;i<cities.length;i++){
+        let latitude = cities[i].latitude;
+        let longitude = cities[i].longitude;
+        let positieX = (~~longitude + 180)*2;
+        let positieY = (90-~~latitude)*2;
+
+        d3.select("svg")
+        .append("rect")
+        .attr("x", positieX)
+        .attr("y", positieY)
+        .attr("width", 1.5)
+        .attr("height", 1.5)
+        .attr("fill", "lightblue")
+    }
+}
+
+function changeCircle(){
+
+    d3.selectAll("rect").remove()
+
+    for(let i=0;i<cities.length;i++){
+        let latitude = cities[i].latitude;
+        let longitude = cities[i].longitude;
+        let positieX = (~~longitude + 180)*2;
+        let positieY = (90-~~latitude)*2;
+
+        d3.select("svg")
+        .append("circle")
+        .attr("cx", positieX)
+        .attr("cy", positieY)
+        .attr("r", .5)
+        .attr("fill", "lightblue")
+    }
+}
